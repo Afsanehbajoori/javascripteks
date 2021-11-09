9063f024ea25fe371a8008a14c9e4699cbfdb203
Afsaneh Bajoori <afsan.bajoori@hotmail.com>
2021-11-09 09:28:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+var takeItems = document.getElementById('inputList');
+var btn = document.getElementById('btn');
+//var lableTxt=document.getElementById("lableItem");
+//console.log(lableTxt);
+var list = document.getElementById('list');
+var item_list = document.getElementById('item-list');
+var btnClear = document.getElementById('btnClear');
+console.log(takeItems);
+
+
+
+
+btn.addEventListener('click' , (e) =>{
+    console.log(takeItems.value);
+    //console.log(list.value);
+    //lableTxt.value=takeItems.value;
+    //list.textContent=takeItems.value;
+    //list.append(takeItems.value);
+    var txt= takeItems.value;
+    
+    var li = document.createElement('li');
+    var ch = document.createElement('input');
+    ch.type="checkbox";
+    ch.name="name";
+    ch.value="value";
+    ch.id="id";
+    
+    li.appendChild(ch);
+    li.innerHTML = txt;
+    list.append(li);
+    
+
+})
+
+
+btnClear.addEventListener('click' , (e) =>{
+    
+    list.remove();
+    
+})
